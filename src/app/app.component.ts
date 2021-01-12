69a466466abd822b3be034c168efcbfdc4bd4d24
pavanpagan <ahitisham.a@gmail.com>
2021-01-13 01:00:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+ 
+  cards = [{
+    url:'../../assets/image/tree.jpeg',
+    title:'Neat Tree',
+    subTitle:'nature',
+    text:'I saw this neat tree today.'
+  },
+  {
+    url:'../../assets/image/mountain.jpeg',
+    title:'Snowy Mountain',
+    subTitle:'maountianlover',
+    text:'Here is a picture of a snowy mountian.'
+  },
+  {
+    url:'../../assets/image/cycling.jpeg',
+    title:'Mountain Biking',
+    subTitle:'biking12222',
+    text:'I did some biking today.'
+  },
+  {
+    url:'../../assets/image/cycling.jpeg',
+    title:'Mountain Biking',
+    subTitle:'biking12222',
+    text:'I did some biking today.'
+  }
+  ]
 }
